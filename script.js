79b39288faaef727d27a05046a7ac408e9530f3a
Saurabh <saurabhsingh4778698@gmail.com>
2023-04-04 22:58:56 +0530

    completed getElementByClassnameHierarchy polyfill

--- script.js
+++ script.js
@@ -1,22 +1,48 @@
-let arr=[];
-function getByClassName(root, className) {
-    
-   
-         if (root.classList.contains(className)) {
-            arr.push(root)
-         }
-         if (root.children.length >0) {
-            for (const child of root.children) {
-               getByClassName(child,className)
+// let arr=[];
+// function getByClassName(root, className) {
+
+//          if (root.classList.contains(className)) {
+//             arr.push(root)
+//          }
+//          if (root.children.length >0) {
+//             for (const child of root.children) {
+//                getByClassName(child,className)
+//             }
+//          } else {
+//             return;
+//          }
+//          return arr;
+
+// }
+var arr=[];
+function getByClassNameHierarchy(root, target,arr) {
+  let parent = target[0];
+  let child = target[2];
+  if (root.children.length<=0) {
+   return;
+  }
+  
+   if (root.classList.contains(parent)) {
+      
+         Array.from(root.children).forEach((e)=>{
+            if (e.classList.contains(child)) {
+               arr.push(e);
+               
             }
-         } else {
-            return;
-         }
-         return arr;
+            getByClassNameHierarchy(e,target ,arr);
+         })
      
-   
+      
+  }
+  else{
+   Array.from(root.children).forEach((e)=>{
+     
+      getByClassNameHierarchy(e,target,arr);
+   })
+  }
+  return arr;
 }
 const getIds = (elements = []) => Array.from(elements).map((x) => x.id);
-const root = document.getElementById("root");
-console.log("actual: ", getIds(getByClassName(root, "a")));
-console.log("expected: ", `['root','a-2','a-3']`);
+const root = document.getElementById("a-1");
+console.log("actual: ", getIds(getByClassNameHierarchy(root, "b>a",arr)));
+console.log("expected: ", `['b-1']`);
